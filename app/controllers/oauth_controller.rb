7dd0a712ac282ecd761ba8558e74b75a202f3502
shafiul <shafiulshaon@gmail.com>
2023-03-15 08:09:04 +0900

    OAuth認可ページの表示

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -1,6 +1,14 @@
 class OauthController < ApplicationController
 
   def connect
+    uri = URI(oauth_uri[:authorization_uri])
+    params = {client_id: oauth_params[:client_id],
+              response_type: oauth_params[:response_type],
+              redirect_uri: oauth_params[:redirect_uri],
+              scope: oauth_params[:scope],
+              state: oauth_params[:state]}
+    uri.query = params.to_query
+    redirect_to uri.to_s
   end
 
   def callback
