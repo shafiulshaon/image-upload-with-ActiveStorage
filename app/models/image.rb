b4f501008fac80acd06d9d12d6f7e0fd77f23a77
shafiul <shafiulshaon@gmail.com>
2023-03-15 07:23:01 +0900

    ユーザーとイメージモデルにバリデーション追加

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,6 @@
 class Image < ApplicationRecord
   belongs_to :user
   has_one_attached :image_file
+  validates :title, presence: true, length: {maximum: 30}
+  validates :image_file, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
+
+  #relation
   has_many :images
+
+  #validation
+  validates :username, uniqueness: true, presence: true
+  validates :password, presence: true
 end
