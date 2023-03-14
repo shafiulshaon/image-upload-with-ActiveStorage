b98e7601c71c665fd5307a2338f2f2dbf75f4f76
shafiul <shafiulshaon@gmail.com>
2023-03-15 07:37:59 +0900

    写真一覧ページ作成。コントローラーとルート追加。

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root to: 'sessions#new'
 
+  resources :images, only: [:index, :new, :create]
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy', as: 'logout'
