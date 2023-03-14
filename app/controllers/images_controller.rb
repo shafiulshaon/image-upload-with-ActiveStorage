9f1c8c34f28e8b049cf3c7bb1c6a84094f1bda89
shafiul <shafiulshaon@gmail.com>
2023-03-15 07:49:15 +0900

    画像ファイルとタイトルをバリデーションし、アップロードするためコントローラー機能修正

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,12 +1,27 @@
 class ImagesController < ApplicationController
 
   def new
+    @image = current_user.images.build
   end
 
   def index
   end
 
   def create
+    @image = current_user.images.build(image_params)
+    if @image.save
+      redirect_to images_path
+    else
+      @errors = []
+      @errors.concat(@image.errors.full_messages.each{|msg|msg})
+      render 'new'
+    end
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:title, :image_file)
   end
 
 
