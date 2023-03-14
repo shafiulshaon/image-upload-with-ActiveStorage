ea82417f35105df4d720b09fa5621cdf56109865
shafiul <shafiulshaon@gmail.com>
2023-03-15 08:09:45 +0900

    OAuthアクセストークンの取得

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -1,3 +1,5 @@
+require 'net/http'
+
 class OauthController < ApplicationController
 
   def connect
@@ -12,6 +14,27 @@ class OauthController < ApplicationController
   end
 
   def callback
+    if params[:code]
+      uri = URI(oauth_uri[:access_token_uri])
+      res = Net::HTTP.post_form(uri, {
+        code: params[:code],
+        client_id: oauth_params[:client_id],
+        client_secret: oauth_params[:client_secret],
+        redirect_uri: oauth_params[:redirect_uri],
+        grant_type: oauth_params[:grant_type]
+      })
+      if res.is_a?(Net::HTTPSuccess)
+        data = JSON.parse(res.body)
+        session[:access_token] = data['access_token']
+        redirect_to images_path
+      else
+        flash[:error] = 'Failed to get access token'
+        redirect_to images_path
+      end
+    else
+      flash[:error] = 'Missing authorization code'
+      redirect_to images_path
+    end
   end
 
   def tweet
