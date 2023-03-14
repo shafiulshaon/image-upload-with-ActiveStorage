c3e8745b9f6436cc1013785e5ce53b023d786e57
shafiul <shafiulshaon@gmail.com>
2023-03-15 06:59:46 +0900

    ログインページ、セッションコントローラーを作成し、ルートを追加した。

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
 
+  root to: 'sessions#new'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy', as: 'logout'
+
 end
