8b443a8a91ea293c8182f9091c9bf2fdaa8860b5
shafiul <shafiulshaon@gmail.com>
2023-03-15 07:12:31 +0900

    ログインページのバリデーション追加。セッションにユーザーIDをセット。

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(user_id: params[:session][:user_id])
+    if @user&.authenticate(params[:session][:password])
+      session[:user_id] = @user.user_id
+      redirect_to images_path
+    else
+      @errors = []
+      @errors.concat(["ユーザーIDを入力してください"]) if params[:session][:user_id].blank?
+      @errors.concat(["パスワードを入力してください"]) if params[:session][:password].blank?
+      @errors.concat(['ユーザーIDとパスワードが一致するユーザーが存在しない']) if params[:session][:user_id].present? && params[:session][:password].present?
+      render 'new'
+    end
   end
 
   def destroy
