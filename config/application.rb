68e871d29aa6da736f3767090d7b2fc0f04c5e76
shafiul <shafiulshaon@gmail.com>
2023-03-15 07:42:23 +0900

    ロケールの構成。ja.yml ファイル追加。

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module PhotoManagementApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.i18n.default_locale = :ja
+    config.i18n.available_locales = [:ja, :en]
 
     # Configuration for the application, engines, and railties goes here.
     #
