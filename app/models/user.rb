272ee0eafcc7f89bbe1abdad75e1b7b2631328ef
Shafiul Alam <shafiulshaon@gmail.com>
2023-03-16 15:13:23 +0900

    ユーザーモデルのattribute名修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
   has_many :images
 
   #validation
-  validates :username, uniqueness: true, presence: true
+  validates :user_id, uniqueness: true, presence: true
   validates :password, presence: true
 end
